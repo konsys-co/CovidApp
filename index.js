9c7b105185304b1501bf825f5dbd21ed81b2f265
Ton Vudhidej <vudhidej@rentspree.com>
2020-03-20 23:29:41 +0700

    init ApolloClient & ApolloProvider

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
+import React from 'react'
 import { AppRegistry } from 'react-native'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { ApolloClient } from 'apollo-client'
+import { ApolloProvider } from '@apollo/react-hooks'
 import App from './src/App'
 import { name as appName } from './app.json'
 
-AppRegistry.registerComponent(appName, () => App)
+// TODO: integrate with production uri
+const link = new HttpLink({ uri: 'http://localhost:3000/graphql' })
+const cache = new InMemoryCache()
+const client = new ApolloClient({ link, cache  })
+
+const Root = () => (
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+)
+
+AppRegistry.registerComponent(appName, () => Root)
