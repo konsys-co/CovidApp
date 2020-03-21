7398a74048758bb2ed25ee1933c70995c84ea1a1
Natthapon Sirinon <natthapon.sirinon@gmail.com>
2020-03-21 16:41:09 +0700

    add prettier for dev-experience

--- src/pages/Scanner/index.js
+++ src/pages/Scanner/index.js
@@ -1,33 +1,26 @@
 import React from 'react'
-import {
-  StyleSheet,
-  View,
-  Text,
-  TouchableOpacity,
-  Linking
-} from 'react-native'
+import { StyleSheet, View, Text, TouchableOpacity, Linking } from 'react-native'
 import QRCodeScanner from 'react-native-qrcode-scanner'
 import { RNCamera as Camera } from 'react-native-camera'
 import * as STATUS from '../../constants/userStatus'
-
 
 const styles = StyleSheet.create({
   centerText: {
     flex: 1,
     fontSize: 18,
     padding: 32,
-    color: '#777'
+    color: '#777',
   },
   textBold: {
     fontWeight: '500',
-    color: '#000'
+    color: '#000',
   },
   buttonText: {
     fontSize: 21,
-    color: 'rgb(0,122,255)'
+    color: 'rgb(0,122,255)',
   },
   buttonTouchable: {
-    padding: 16
+    padding: 16,
   },
   title: {
     fontFamily: 'Kanit-Regular',
@@ -43,9 +36,7 @@ const styles = StyleSheet.create({
 
 const QRScanner = () => {
   const onSuccess = e => {
-    Linking.openURL(e.data).catch(err =>
-      console.error('An error occured', err)
-    )
+    Linking.openURL(e.data).catch(err => console.error('An error occured', err))
   }
 
   return (
@@ -54,7 +45,9 @@ const QRScanner = () => {
       flashMode={Camera.Constants.FlashMode.auto}
       topContent={
         <View>
-          <Text style={{ ...styles.title, color: STATUS.NORMAL.NORMAL }}>{STATUS.TEXT.NORMAL}</Text>
+          <Text style={{ ...styles.title, color: STATUS.NORMAL.NORMAL }}>
+            {STATUS.TEXT.NORMAL}
+          </Text>
           <Text style={styles.subtitle}>แสกนเพื่อบันทึกว่าเราเจอกัน</Text>
         </View>
       }
