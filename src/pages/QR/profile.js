2d4e67c8a2d2ae6ab5e66817fdfbabc7479a8307
Ton Vudhidej <vudhidej@rentspree.com>
2020-03-26 20:43:51 +0700

    change profile to Profile, adjust logout function to be working

--- src/pages/QR/profile.js
+++ src/pages/QR/profile.js
@@ -2,71 +2,13 @@ import React from 'react'
 import moment from 'moment'
 import { Text, View, StyleSheet, Image, Button } from 'react-native'
 import AsyncStorage from '@react-native-community/async-storage'
-import { Icon } from 'react-native-elements'
+import { AntDesign } from '@expo/vector-icons'
+
 import { COLOR } from '../../constants/theme'
 import GradientBackground from '../../components/background'
 import * as STATUS_COLOR from '../../constants/userStatus'
 
-const logout = () => {
-  AsyncStorage.removeItem('@FacebookOAuthKey:accessToken')
-  // setLoggedinStatus(false)
-  // setUserData(null)
-  // setImageLoadStatus(false)
-}
-
 const status = 'NORMAL' // TODO: Fetch from server later.
-
-export default ({ userData, navigation }) => (
-  <View style={styles.container}>
-    <GradientBackground status={status} style={{ paddingHorizontal: 20 }}>
-      <View style={styles.spaceBetweenRow}>
-        <>
-          {/* <Icon name='fa-times' type='font-awesome' /> */}
-          <Text onPress={() => navigation.navigate('QR')}>Back</Text>
-        </>
-      </View>
-      <View style={{ width: '100%' }}>
-        <View style={styles.cardWrapper}>
-          <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-            <Image style={styles.avatar} source={{ uri: userData.picture.data.url }} />
-            <View style={{ marginLeft: 20, justifyContent: 'space-around' }}>
-              <Text style={{ ...styles.title, color: STATUS_COLOR.NORMAL[status] }}>{userData.name}</Text>
-              <Text style={styles.subtitle}>เริ่มใช้งานเมื่อ {moment().fromNow()}</Text>
-            </View>
-          </View>
-        </View>
-        <View style={{ flexDirection: 'row' }}>
-          <View style={{ borderBottomColor: COLOR.LINE_GRAY, borderBottomWidth: 2, flex: 1, height: 15 }} />
-          <Text style={{ ...styles.subtitle, paddingHorizontal: 8 }}>ประวัติการแสกน</Text>
-          <View style={{ borderBottomColor: COLOR.LINE_GRAY, borderBottomWidth: 2, flex: 1, height: 15 }} />
-        </View>
-        <View style={{ flexDirection: 'column', width: '100%', }}>
-          <View style={styles.spaceBetweenRow}>
-            <Text style={styles.subtitle}>คนที่พบทั้งหมด</Text>
-            <Text style={styles.subtitle}>75</Text>
-          </View>
-          <View style={styles.spaceBetweenRow}>
-            <Text style={styles.subtitle}>สุขภาพปกติ</Text>
-            <Text style={{ ...styles.subtitle }}>75</Text>
-          </View>
-          <View style={styles.spaceBetweenRow}>
-            <Text style={styles.subtitle}>เฝ้าระวัง</Text>
-            <Text style={{ ...styles.subtitle }}>10</Text>
-          </View>
-          <View style={styles.spaceBetweenRow}>
-            <Text style={styles.subtitle}>มีเชื้อ COVID-19</Text>
-            <Text style={{ ...styles.subtitle }}>5</Text>
-          </View>
-          <View style={styles.spaceBetweenRow}>
-            <Text style={styles.subtitle}>รักษาหายแล้ว</Text>
-            <Text style={{ ...styles.subtitle }}>19</Text>
-          </View>
-        </View>
-        <Button onPress={() => logout()} title='ออกจากระบบ' />
-      </View>
-    </GradientBackground>
-  </View>
-)
 
 const styles = StyleSheet.create({
   container: {
@@ -83,26 +25,119 @@ const styles = StyleSheet.create({
     paddingVertical: 16,
     paddingHorizontal: 10,
   },
+  title: {
+    fontFamily: 'Kanit-Regular',
+    alignSelf: 'flex-start',
+    fontSize: 24,
+  },
   avatar: {
     width: 50,
     height: 50,
     borderRadius: 50 / 2,
   },
-  title: {
+  profileName: {
     fontSize: 20,
-    // fontWeight: '300',
     fontFamily: 'Kanit-Regular',
   },
-  subtitle: {
+  text: {
     fontSize: 16,
-    // fontWeight: '100',
     fontFamily: 'Kanit-Regular',
-    color: COLOR.TEXT_GRAY
+    color: COLOR.TEXT_GRAY,
   },
   spaceBetweenRow: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     width: '100%',
-    marginVertical: 8
-  }
+    marginVertical: 8,
+  },
 })
+
+const Profile = ({ navigation, userData, setLoggedinStatus, setUserData }) => {
+  const logout = () => {
+    AsyncStorage.removeItem('@FacebookOAuthKey:accessToken')
+    setLoggedinStatus(false)
+    setUserData(null)
+  }
+
+  return (
+    <View style={styles.container}>
+      <GradientBackground status={status} style={{ paddingHorizontal: 20 }}>
+        <View style={styles.spaceBetweenRow}>
+          <Text
+            onPress={() => navigation.navigate('QRDetail')}
+            style={styles.title}>
+            <AntDesign name="left" size={24} /> ข้อมูลส่วนตัว
+          </Text>
+        </View>
+        <View style={{ width: '100%' }}>
+          <View style={styles.cardWrapper}>
+            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+              <Image
+                style={styles.avatar}
+                source={{ uri: userData.picture.data.url }}
+              />
+              <View style={{ marginLeft: 20, justifyContent: 'space-around' }}>
+                <Text
+                  style={{
+                    ...styles.profileName,
+                    color: STATUS_COLOR.NORMAL[status],
+                  }}>
+                  {userData.name}
+                </Text>
+                <Text style={styles.text}>
+                  เริ่มใช้งานเมื่อ {moment().fromNow()}
+                </Text>
+              </View>
+            </View>
+          </View>
+          <View style={{ flexDirection: 'row' }}>
+            <View
+              style={{
+                borderBottomColor: COLOR.LINE_GRAY,
+                borderBottomWidth: 2,
+                flex: 1,
+                height: 15,
+              }}
+            />
+            <Text style={{ ...styles.text, paddingHorizontal: 8 }}>
+              ประวัติการแสกน
+            </Text>
+            <View
+              style={{
+                borderBottomColor: COLOR.LINE_GRAY,
+                borderBottomWidth: 2,
+                flex: 1,
+                height: 15,
+              }}
+            />
+          </View>
+          <View style={{ flexDirection: 'column', width: '100%' }}>
+            <View style={styles.spaceBetweenRow}>
+              <Text style={styles.text}>คนที่พบทั้งหมด</Text>
+              <Text style={styles.text}>75</Text>
+            </View>
+            <View style={styles.spaceBetweenRow}>
+              <Text style={styles.text}>สุขภาพปกติ</Text>
+              <Text style={{ ...styles.text }}>75</Text>
+            </View>
+            <View style={styles.spaceBetweenRow}>
+              <Text style={styles.text}>เฝ้าระวัง</Text>
+              <Text style={{ ...styles.text }}>10</Text>
+            </View>
+            <View style={styles.spaceBetweenRow}>
+              <Text style={styles.text}>มีเชื้อ COVID-19</Text>
+              <Text style={{ ...styles.text }}>5</Text>
+            </View>
+            <View style={styles.spaceBetweenRow}>
+              <Text style={styles.text}>รักษาหายแล้ว</Text>
+              <Text style={{ ...styles.text }}>19</Text>
+            </View>
+          </View>
+          <Button onPress={() => logout()} title="ออกจากระบบ" />
+        </View>
+      </GradientBackground>
+    </View>
+  )
+}
+
+export default Profile
