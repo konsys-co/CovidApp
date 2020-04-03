ad65b50247cc8e5f335c7790e61e2f44840307b4
Ton Vudhidej <vudhidej@rentspree.com>
2020-04-03 18:10:23 +0700

    implement new PushNotificationManager

--- src/components/PushNotificationManager.js
+++ src/components/PushNotificationManager.js
@@ -1,91 +1,75 @@
-// import React from 'react'
-// import { Platform, View } from 'react-native'
-// import AsyncStorage from '@react-native-community/async-storage'
-// import { Notifications } from 'react-native-notifications'
+import React, { useEffect } from 'react'
+import { Platform, View } from 'react-native'
+import fcm from '@react-native-firebase/messaging'
+import AsyncStorage from '@react-native-community/async-storage'
 
-// const DEVICE_TOKEN_TYPE = {
-//   ios: 'APNS',
-//   android: 'FCM',
-// }
+const DEVICE_TOKEN_TYPE = {
+  ios: 'APNS',
+  android: 'FCM',
+}
 
-// export default class PushNotificationManager extends React.Component {
-//   componentDidMount() {
-//     console.log('PushNotificationManager')
-//     this.registerDevice()
-//     this.registerNotificationEvents()
-//   }
+const messaging = fcm()
 
-//   registerDevice = async () => {
-//     const userToken = await AsyncStorage.getItem(
-//       '@TidyoungUserToken:accessToken',
-//     )
-//     Notifications.events().registerRemoteNotificationsRegistered(
-//       ({ deviceToken }) => {
-//         // TODO: Send the token to my server so it could send back push notifications...
-//         console.log('Device Token Received ===>', deviceToken)
-//         console.log('Platform ===>', Platform.OS)
-//         console.log('Token Type ===>', DEVICE_TOKEN_TYPE[Platform.OS])
-//         // eslint-disable-next-line no-undef
-//         fetch('https://tidyoung.devspree.xyz/notifications/push-device', {
-//           method: 'POST',
-//           headers: {
-//             'Content-Type': 'application/json',
-//             Authorization: userToken ? `Bearer ${userToken}` : '',
-//           },
-//           body: JSON.stringify({
-//             deviceToken,
-//             type: DEVICE_TOKEN_TYPE[Platform.OS],
-//           }),
-//         })
-//       },
-//     )
-//     Notifications.events().registerRemoteNotificationsRegistrationFailed(
-//       event => {
-//         console.error(event)
-//       },
-//     )
+const PushNotificationManager = ({ children }) => {
+  const registerAppWithFCM = async () => {
+    await messaging.registerForRemoteNotifications()
 
-//     Notifications.registerRemoteNotifications()
-//   }
+    const deviceToken = await messaging.getToken()
+    console.log('Received device token :', deviceToken)
 
-//   registerNotificationEvents = async () => {
-//     Notifications.events().registerNotificationReceivedForeground(
-//       (notification, completion) => {
-//         console.log('Notification Received - Foreground', notification)
-//         // Calling completion on iOS with `alert: true` will present the native iOS inApp notification.
-//         completion({ alert: true, sound: true, badge: true })
-//       },
-//     )
+    const userToken = await AsyncStorage.getItem(
+      '@TidyoungUserToken:accessToken',
+    )
+    const body = JSON.stringify({
+      deviceToken,
+      type: DEVICE_TOKEN_TYPE[Platform.OS],
+    })
+    console.log('/push-device body :', body)
 
-//     Notifications.events().registerNotificationOpened(
-//       (notification, completion) => {
-//         console.log('Notification opened by device user', notification)
-//         console.log(
-//           `Notification opened with an action identifier: ${notification.identifier}`,
-//         )
-//         completion()
-//       },
-//     )
+    // eslint-disable-next-line no-undef
+    fetch('https://tidyoung.devspree.xyz/notifications/push-device', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: userToken ? `Bearer ${userToken}` : '',
+      },
+      body,
+    })
+  }
 
-//     Notifications.events().registerNotificationReceivedBackground(
-//       (notification, completion) => {
-//         console.log('Notification Received - Background', notification)
+  const requestPermission = () => {
+    const granted = messaging.requestPermission()
+    if (granted) {
+      console.log('User granted messaging permissions!')
+    } else {
+      console.log('User declined messaging permissions :(')
+    }
+  }
 
-//         // Calling completion on iOS with `alert: true` will present the native iOS inApp notification.
-//         completion({ alert: true, sound: true, badge: true })
-//       },
-//     )
+  useEffect(() => {
+    const registerNotification = async () => {
+      await registerAppWithFCM()
+      requestPermission()
+    }
+    registerNotification()
 
-//     try {
-//       const notification = await Notifications.getInitialNotification()
-//       console.log('Initial notification was:', notification || 'N/A')
-//     } catch (err) {
-//       console.error('getInitialNotifiation() failed', err)
-//     }
-//   }
+    // Background messages
+    messaging.setBackgroundMessageHandler(async remoteMessage => {
+      console.log('Message handled in the background!', remoteMessage)
+    })
 
-//   render() {
-//     const { children } = this.props
-//     return <View style={{ flex: 1 }}>{children}</View>
-//   }
-// }
+    // Foreground messages
+    const unregisterOnMessage = messaging.onMessage(async remoteMessage => {
+      console.log('Message handled in the foreground!', remoteMessage)
+    })
+
+    return () => {
+      unregisterOnMessage()
+      messaging.unregisterForRemoteNotifications()
+    }
+  }, [])
+
+  return <View style={{ flex: 1 }}>{children}</View>
+}
+
+export default PushNotificationManager
