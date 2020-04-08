3f2626e191ca2c08f39f1ad78a4b42c05d36d626
Natthapon Sirinon <natthapon.sirinon@gmail.com>
2020-04-08 15:17:42 +0700

    add modal for notification foreground

--- src/components/PushNotificationManager.js
+++ src/components/PushNotificationManager.js
@@ -1,7 +1,12 @@
-import React, { useEffect } from 'react'
-import { Platform, View, Alert } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { Platform, View, Text, StyleSheet } from 'react-native'
+import { Button } from 'react-native-elements'
+import Modal from 'react-native-modal'
 import fcm from '@react-native-firebase/messaging'
 import AsyncStorage from '@react-native-community/async-storage'
+
+import * as STATUS from '../constants/userStatus'
+import { COLOR } from '../constants/theme'
 
 const DEVICE_TOKEN_TYPE = {
   ios: 'APNS',
@@ -10,7 +15,53 @@ const DEVICE_TOKEN_TYPE = {
 
 const messaging = fcm()
 
+const styles = StyleSheet.create({
+  notiWrapper: {
+    flex: 0.4,
+    borderRadius: 15,
+    backgroundColor: 'white',
+  },
+  notiContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  notiTitle: {
+    fontFamily: 'Kanit-Regular',
+    fontSize: 26,
+    marginBottom: 20,
+    color: COLOR.BLUE,
+  },
+  notiMsg: {
+    fontFamily: 'Kanit-Regular',
+    fontSize: 16,
+    marginBottom: 20,
+    color: COLOR.BLACK,
+  },
+  btnContainer: {
+    display: 'flex',
+    width: '100%',
+  },
+  button: {
+    width: '85%',
+    alignSelf: 'center',
+    backgroundColor: 'transparent',
+    borderWidth: 3,
+    borderRadius: 10,
+    marginBottom: 15,
+  },
+  btnText: {
+    color: '#000',
+    fontFamily: 'Kanit-Regular',
+    fontSize: 20,
+  },
+})
+
 const PushNotificationManager = ({ children }) => {
+  const [isVisible, setIsVisible] = useState(false)
+  const [notiTitle, setNotitle] = useState(null)
+  const [notiMsg, setNotiMsg] = useState(null)
+
   console.log('PushNotificationManager Running...')
   const registerAppWithFCM = async () => {
     await messaging.registerForRemoteNotifications()
@@ -62,7 +113,13 @@ const PushNotificationManager = ({ children }) => {
     // Foreground messages
     const unregisterOnMessage = messaging.onMessage(async remoteMessage => {
       console.log('Message handled in the foreground!', remoteMessage)
-      // Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage))
+      const notiPayload = remoteMessage.data
+      const { notification } = notiPayload
+      const { title } = notification
+      const { body } = notification
+      setNotitle(title)
+      setNotiMsg(body)
+      setIsVisible(true)
     })
 
     return () => {
@@ -71,7 +128,37 @@ const PushNotificationManager = ({ children }) => {
     }
   }, [])
 
-  return <View style={{ flex: 1 }}>{children}</View>
+  return (
+    <View style={{ flex: 1 }}>
+      {children}
+      <View>
+        <Modal
+          isVisible={isVisible}
+          onBackdropPress={() => setIsVisible(false)}
+          onBackButtonPress={() => setIsVisible(false)}>
+          <View style={styles.notiWrapper}>
+            <View style={styles.notiContainer}>
+              <Text style={styles.notiTitle}>TidYoung Notification</Text>
+              <Text style={styles.notiMsg}>เพื่อนของคุณ</Text>
+              <Text style={styles.notiMsg}>{notiTitle}</Text>
+              <Text style={styles.notiMsg}>{notiMsg}</Text>
+              <View style={styles.btnContainer}>
+                <Button
+                  title="รับรู้และเช็คอาการตัวเอง"
+                  titleStyle={styles.btnText}
+                  buttonStyle={{
+                    ...styles.button,
+                    borderColor: STATUS.NORMAL.normal,
+                  }}
+                  onPress={() => setIsVisible(false)}
+                />
+              </View>
+            </View>
+          </View>
+        </Modal>
+      </View>
+    </View>
+  )
 }
 
 export default PushNotificationManager
